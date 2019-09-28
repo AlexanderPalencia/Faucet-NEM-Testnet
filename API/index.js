94b20df448cfcbc06a3c33a698fa3967bdedc6d2
AlexanderPalencia <mapg23@outlook.com>
2019-09-28 11:50:48 -0600

    API Logic

--- API/index.js
+++ API/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
-require('dotenv').config({path: './config.env'});
+require('dotenv').config({path: '../config.env'});
+let nem = require("nem-sdk").default;
 var app = express();
 
 app.get('/', function (req, res) {
   res.send('API Alex, put a valid end-point');
+});
+
+app.get('/faucet', function (req, res) {
+  let Address = req.query.add;
+
+  res.send('Holis');
 });
 
 app.listen(process.env.port, function () {
