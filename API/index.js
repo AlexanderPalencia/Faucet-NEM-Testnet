56e1709b73190c003dbd3238b6d019c8b797484c
AlexanderPalencia <mapg23@outlook.com>
2019-09-28 12:38:13 -0600

    Add transaction funtion to API

--- API/index.js
+++ API/index.js
@@ -8,9 +8,34 @@ app.get('/', function (req, res) {
 });
 
 app.get('/faucet', function (req, res) {
-  let Address = req.query.add;
+  // Queriy Params
+  let address = req.query.add;
+  let msg = req.query.msg;
+  let randomAmount = Math.floor((Math.random() * 20) + 1);
 
-  res.send('Holis');
+  // Transfer XEM
+  let endpoint  = nem.model.objects.create("endpoint")(nem.model.nodes.defaultTestnet, nem.model.nodes.defaultPort);
+  let objCommon = nem.model.objects.create("common")("", process.env.PK);
+  // Params(addressRecipient, amount, Message)
+  let transferTransaction = nem.model.objects.create("transferTransaction")(address, randomAmount, msg);
+  let prepare = nem.model.transactions.prepare("transferTransaction")(objCommon, transferTransaction, nem.model.network.data.testnet.id);
+  
+  nem.com.requests.chain.time(endpoint).then((timeStamp) => {
+    const ts = Math.floor(timeStamp.receiveTimeStamp / 1000);
+    prepare.timeStamp = ts;
+    const due = 60;
+    prepare.deadline = ts + due * 60;
+    console.log("New TimeStampt",ts);
+    // sendParams(commonObj, prepareObj, endpointObj)
+    nem.model.transactions.send(objCommon, prepare, endpoint).then((resTran)=>{
+      console.log("Success transaction", resTran);
+      res.send('Holis');
+    },(err)=>{
+        console.log(err);
+    });
+  }, (err)=>{
+    console.log(err);
+  });
 });
 
 app.listen(process.env.port, function () {
