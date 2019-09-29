67da3f4f2928d67bfdaf8e6ca2826aa866c72746
AlexanderPalencia <mapg23@outlook.com>
2019-09-28 21:00:58 -0600

    add logic

--- Client/index.js
+++ Client/index.js
@@ -8,32 +8,41 @@ async function myFunction(){
     // TBRN3RHVYI4JKOUD5BQAN5XQOYRK6PJTDCFL3R7J
     // http://localhost:3000/faucet?add=TBRN3RHVYI4JKOUD5BQAN5XQOYRK6PJTDCFL3R7J&msg=Soy%20la%20Puta%20ley
     if (patt.test(address) && patt2.test(address)){
+        let DOMAddError = document.getElementById("addError");
         if(patt1.test(address)){
             alert("Invalid address");
+            DOMAddError.hidden = false;
         }else{
             msg = encodeURIComponent(msg.trim());
             let uri = `http://localhost:3000/faucet?add=${address}&msg=${msg}`;
             fetch(uri)
             .then(response => response.json())
             .then(jsonObj => {
-                alert(JSON.stringify(jsonObj.error["data"].error));
+                let DOMError = document.getElementById("error");
+                let DOMSucc = document.getElementById("succ");
+                alert(JSON.stringify(jsonObj));
                 if(jsonObj.error){
-                    let DOMError = document.getElementById("error");
+                    DOMSucc.hidden = true;
+                    DOMAddError.hidden = true;
                     let DOMErrorCode = document.getElementById("errorCode");
                     let DOMErrorMsg = document.getElementById("erroMsg");
                     DOMErrorCode.innerText = jsonObj.error["data"].error;
                     DOMErrorMsg.innerText = jsonObj.error["data"].message;
                     DOMError.hidden = false;
-                    alert("error joven e ");
-                }else{  
-                    alert("Valid Transaction");
-                    let DOMInfo = document.getElementById("info");
-                    DOMInfo.innerText = "aqui perro";            
-                    DOMInfo.hidden = false;
+                }else{
+                    DOMError.hidden = true;
+                    DOMAddError.hidden = true;
+                    let DOMAmount = document.getElementById("amount");
+                    let DOMToAdd = document.getElementById("toadd");
+                    DOMAmount.innerText = jsonObj.amount;
+                    DOMToAdd.innerText = jsonObj.recipient;
+                    DOMSucc.hidden = false;
                 }
             });
         }
     }else{
-        alert("Invalid address")
+        alert("Invalid address");
+        let DOMAddError = document.getElementById("addError");
+        DOMAddError.hidden = false;
     }
 }
